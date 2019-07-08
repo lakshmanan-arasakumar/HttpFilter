097a5544c2a5a0a9cb5738ef5c90790258d5cebc
Lakshmanan Arasakumar <karthik-dhaaru@Lakshmanans-MacBook-Pro.local>
2019-07-08 07:54:51 -0400

    Interceptor added

--- src/main/java/filter/RequestFilter.java
+++ src/main/java/filter/RequestFilter.java
@@ -1,68 +1,63 @@
 package filter;
 
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.UUID;
 
 
 @Component
 public class RequestFilter implements Filter{
-    private static final Logger logger = LoggerFactory.getLogger(RequestFilter.class);
-    public static final String CORRELATION_ID_KEY = "correlationId";
-    public static final String APP_ID_KEY = "X-Appid";
+	private static final Logger logger = LoggerFactory.getLogger(RequestFilter.class);
+	public static final String CORRELATION_ID_KEY = "correlationId";
+	public static final String APP_ID_KEY = "X-Appid";
 
-    //@Value("${X-Appid}")
-    String appId = "dummyAppId";
-    
-    /*@Value("${enable.http.filter}")
-    boolean enableHttpFilter;*/
+	//@Value("${X-Appid}")
+	String appId = "dummyAppId";
 
-    @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-        logger.debug("*** Filter registered and initialized..");
-    }
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		logger.debug("*** Filter registered and initialized..");
+	}
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        //if (enableHttpFilter == true) {
-        	try{
-                //get the correlationId if its already there
-                HttpServletRequest httpRequest = (HttpServletRequest) request;
-                String newCorrelationId = getCorrelationId(httpRequest);
-                httpRequest.setAttribute(CORRELATION_ID_KEY, newCorrelationId);
-                MDC.put(CORRELATION_ID_KEY, newCorrelationId);
-                MDC.put(APP_ID_KEY, appId);
-                chain.doFilter(request, response);
-            } finally {
-                logger.debug("*** clearing the MDC");
-                MDC.clear();
-            }
-        /*} else {
-        	logger.debug("enableHttpFilter is false, skipping request filter");
-        }*/
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		try{
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+			String newCorrelationId = getCorrelationId(httpRequest);
+			httpRequest.setAttribute(CORRELATION_ID_KEY, newCorrelationId);
+			MDC.put(CORRELATION_ID_KEY, newCorrelationId);
+			MDC.put(APP_ID_KEY, appId);
+			chain.doFilter(request, response);
+		} finally {
+			logger.debug("*** clearing the MDC");
+			MDC.clear();
+		}
+	}
 
-    }
+	private String getCorrelationId(HttpServletRequest request) {
+		String newCorrelationId  = request.getHeader(CORRELATION_ID_KEY);
+		if(newCorrelationId == null){
+			UUID uuid = UUID.randomUUID();
+			newCorrelationId = uuid.toString();
+			logger.info("**** Generating new corelationid: {}" , newCorrelationId);
+		}
+		return newCorrelationId;
+	}
 
-    private String getCorrelationId(HttpServletRequest request) {
-        String newCorrelationId  = request.getHeader(CORRELATION_ID_KEY);
-        if(newCorrelationId == null){
-            // X-correlationid is not present in header, generate new
-            UUID uuid = UUID.randomUUID();
-            newCorrelationId = uuid.toString();
-            logger.info("**** Generating new corelationid: {}" , newCorrelationId);
-        }
-        return newCorrelationId;
-    }
-
-    @Override
-    public void destroy() {
-        logger.debug("*** Filter Destroyed..");
-    }
+	@Override
+	public void destroy() {
+		logger.debug("*** Filter Destroyed..");
+	}
 }
